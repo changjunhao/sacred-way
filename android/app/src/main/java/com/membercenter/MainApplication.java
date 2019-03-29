c196c39fc23c6a8203bbb4a89e10c0befebe3265
junhao <changjunhao@meimiao.net>
2019-03-30 00:45:56 +0800

    add react-native-linear-gradient

--- android/app/src/main/java/com/membercenter/MainApplication.java
+++ android/app/src/main/java/com/membercenter/MainApplication.java
@@ -3,6 +3,7 @@ package com.membercenter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new AsyncStoragePackage()
