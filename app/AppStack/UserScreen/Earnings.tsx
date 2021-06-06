e01db7ddfcdd19d288639e54459097939a6801a4
changjunhao <ward_1992@msn.cn>
2021-06-06 22:21:07 +0800

    U: Earnings FC

--- app/AppStack/UserScreen/Earnings.tsx
+++ app/AppStack/UserScreen/Earnings.tsx
@@ -1,84 +1,97 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import {FlatList, Image, SafeAreaView, Text, View} from 'react-native';
 import {getUserIoList} from '../../Services/distribution';
 import styles from './Styles';
 
-interface InterfaceState {
-  list: any[];
-  page: number;
-  limit: number;
-  count: number;
-}
+const Earnings: React.FC = () => {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit] = useState(50);
+  const [count, setCount] = useState(0);
 
-export default class Earnings extends Component<{}, InterfaceState> {
-  constructor(props: Readonly<{}>) {
-    super(props);
-    this.state = {
-      list: [],
-      page: 1,
-      limit: 50,
-      count: 0,
-    };
-  }
+  useEffect(() => {
+    fetchData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  public componentDidMount() {
-    this.fetchData();
-  }
-
-  public render() {
-    const {list} = this.state;
-
-    return (
-      <SafeAreaView style={{flex: 1}}>
-        <FlatList
-          style={styles.listView}
-          data={list}
-          onEndReached={this.onEndReached}
-          keyExtractor={item => item.id.toString()}
-          ItemSeparatorComponent={() => <View style={styles.itemHr} />}
-          renderItem={({item}) => (
-            <View style={styles.itemView}>
-              <Image source={{uri: item.head_img}} style={styles.avatar} />
-              <View style={{flex: 1}}>
-                <Text style={styles.name}>
-                  {item.nick_name}
-                  <Text style={styles.realName}>（{item.real_name}）</Text>
-                </Text>
-                <Text style={styles.info}>购买时间：{item.create_time}</Text>
-                <Text style={styles.info}>购买课程：{item.name}</Text>
-              </View>
-              <Text style={styles.earningMoney}>+ ¥ {item.price / 100}</Text>
-            </View>
-          )}
-        />
-      </SafeAreaView>
-    );
-  }
-
-  private fetchData = () => {
-    getUserIoList({page: this.state.page, pageSize: this.state.limit}).then(
-      res => {
-        this.setState({
-          list: res.list,
-          count: res.count,
-        });
-      },
-    );
-  };
-
-  private onEndReached = () => {
-    if (this.state.page >= this.state.count / this.state.limit) {
-      return;
-    }
-    this.setState({page: this.state.page + 1}, () => {
-      getUserIoList({page: this.state.page, pageSize: this.state.limit}).then(
-        res => {
-          this.setState({
-            list: this.state.list.concat(res.list),
-            count: res.count,
-          });
-        },
-      );
+  const fetchData = () => {
+    getUserIoList({page, pageSize: limit}).then(res => {
+      setList(res.list);
+      setCount(res.count);
     });
   };
-}
+
+  useEffect(() => {
+    getUserIoList({page, pageSize: limit}).then(res => {
+      setList(page === 1 ? res.list : list.concat(res.list));
+      setCount(res.count);
+    });
+  }, [page]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const onEndReached = () => {
+    if (page >= count / limit) {
+      return;
+    }
+    setPage(page + 1);
+  };
+
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <FlatList
+        style={styles.listView}
+        data={list}
+        onEndReached={onEndReached}
+        // @ts-ignore
+        keyExtractor={item => item.id.toString()}
+        ItemSeparatorComponent={() => <View style={styles.itemHr} />}
+        renderItem={({item}) => (
+          <View style={styles.itemView}>
+            <Image
+              // @ts-ignore
+              source={{uri: item.head_img}}
+              style={styles.avatar}
+            />
+            <View style={{flex: 1}}>
+              <Text style={styles.name}>
+                {
+                  // @ts-ignore
+                  item.nick_name
+                }
+                <Text style={styles.realName}>
+                  （
+                  {
+                    // @ts-ignore
+                    item.real_name
+                  }
+                  ）
+                </Text>
+              </Text>
+              <Text style={styles.info}>
+                购买时间：
+                {
+                  // @ts-ignore
+                  item.create_time
+                }
+              </Text>
+              <Text style={styles.info}>
+                购买课程：
+                {
+                  // @ts-ignore
+                  item.name
+                }
+              </Text>
+            </View>
+            <Text style={styles.earningMoney}>
+              + ¥{' '}
+              {
+                // @ts-ignore
+                item.price / 100
+              }
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Earnings;
